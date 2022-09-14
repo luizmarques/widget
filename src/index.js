c9cd5dd4f6dff19f6291330159bc4e814bc164fd
luizfernando.marques <luizfernando.marques@inatel.br>
2022-09-14 14:23:42 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Find all widget divs
+const WidgetDivs = document.querySelectorAll('.reddit_widget')
+
+// Inject our React App into each
+WidgetDivs.forEach(Div => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App domElement={Div} />
+    </React.StrictMode>,
+    Div
+  );
+})
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
